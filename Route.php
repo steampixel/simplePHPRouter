3a8218687f1d1cfa50e2c86ebc77cdd94992e2a8
steampixel <info@steampixel.de>
2018-03-13 16:25:37 +0100

    Fixed mixed indents

--- Route.php
+++ Route.php
@@ -1,89 +1,89 @@
 <?PHP
 
 class Route{
-	
-	private static $routes = Array();
-	private static $pathNotFound = null;
+
+  private static $routes = Array();
+  private static $pathNotFound = null;
   private static $methodNotAllowed = null;
 
-	public static function add($expression, $function, $method = 'get'){
-		array_push(self::$routes,Array(
-			'expression' => $expression,
-			'function' => $function,
+  public static function add($expression, $function, $method = 'get'){
+    array_push(self::$routes,Array(
+      'expression' => $expression,
+      'function' => $function,
       'method' => $method
-		));
-	}
-  
-	public static function pathNotFound($function){
-		self::$pathNotFound = $function;
-	}
-  
-  public static function methodNotAllowed($function){
-		self::$methodNotAllowed = $function;
-	}
-	
-	public static function run($basepath = '/'){
-		  
-    // Parse current url
-		$parsed_url = parse_url($_SERVER['REQUEST_URI']);//Parse Uri
+    ));
+  }
 
-		if(isset($parsed_url['path'])){
-			$path = $parsed_url['path'];
-		}else{
-			$path = '/';
-		}
-    
+  public static function pathNotFound($function){
+    self::$pathNotFound = $function;
+  }
+
+  public static function methodNotAllowed($function){
+    self::$methodNotAllowed = $function;
+  }
+
+  public static function run($basepath = '/'){
+
+    // Parse current url
+    $parsed_url = parse_url($_SERVER['REQUEST_URI']);//Parse Uri
+
+    if(isset($parsed_url['path'])){
+      $path = $parsed_url['path'];
+    }else{
+      $path = '/';
+    }
+
     // Get current request method
     $method = $_SERVER['REQUEST_METHOD'];
-    
-		$path_match_found = false;
-    
+
+    $path_match_found = false;
+
     $route_match_found = false;
-		
-		foreach(self::$routes as $route){
+
+    foreach(self::$routes as $route){
 
       // If the method matches check the path
-      
+
       // Add basepath to matching string
       if($basepath!=''&&$basepath!='/'){
         $route['expression'] = '('.$basepath.')'.$route['expression'];
       }
-      
+
       // Add 'find string start' automatically
       $route['expression'] = '^'.$route['expression'];
- 
+
       // Add 'find string end' automatically
       $route['expression'] = $route['expression'].'$';
-      
+
       // echo $route['expression'].'<br/>';
-      
+
       // Check path match	
       if(preg_match('#'.$route['expression'].'#',$path,$matches)){
-        
+
         $path_match_found = true;
-        
+
         // Check method match
         if(strtolower($method) == strtolower($route['method'])){
-        
+
           array_shift($matches);// Always remove first element. This contains the whole string
-          
+
           if($basepath!=''&&$basepath!='/'){
             array_shift($matches);// Remove basepath
           }
-          
+
           call_user_func_array($route['function'], $matches);
-          
+
           $route_match_found = true;
-          
+
           // Do not check other routes
           break;
         }
-			}
-		}
-		
+      }
+    }
+
     // No matching route was found
-		if(!$route_match_found){
-			
+    if(!$route_match_found){
+
       // But a matching path exists
       if($path_match_found){
         header("HTTP/1.0 405 Method Not Allowed");
@@ -96,9 +96,9 @@ class Route{
           call_user_func_array(self::$pathNotFound, Array($path));
         }
       }
- 
-		}
-		
-	}
-	
+
+    }
+
+  }
+
 }
