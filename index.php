92af3f3e2ceeba99d6dbb27dffa37335c68f71ad
SteamPixel <info@steampixel.de>
2020-01-10 22:56:48 +0100

    Fixed index.php navigation

--- index.php
+++ index.php
@@ -1,56 +1,64 @@
 <?php
-echo <<<EOD
-Navigation:
-<ul>
-    <li><a href="/">home</a></li>
-    <li><a href="/index.php">index.php</a></li>
-    <li><a href="/user/3/edit">edit user 3</a></li>
-    <li><a href="/foo/5/bar">foo 5 bar</a></li>
-    <li><a href="/foo/bar/foo/bar">long route example</a></li>
-    <li><a href="/contact-form">contact form</a></li>
-    <li><a href="/get-post-sample">get+post example</a></li>
-    <li><a href="/test.html">test.html</a></li>
-    <li><a href="/aTrailingSlashDoesNotMatters">aTrailingSlashDoesNotMatters</a></li>
-    <li><a href="/aTrailingSlashDoesNotMatters/">aTrailingSlashDoesNotMatters/</a></li>
-    <li><a href="/theCaseDoesNotMatters">theCaseDoesNotMatters</a></li>
-    <li><a href="/thecasedoesnotmatters">thecasedoesnotmatters</a></li>
-    <li><a href="/this-route-is-not-defined">404 Test</a></li>
-    <li><a href="/this-route-is-defined">405 Test</a></li>
-</ul>
+function navi() {
+  echo <<<EOD
+  Navigation:
+  <ul>
+      <li><a href="/">home</a></li>
+      <li><a href="/index.php">index.php</a></li>
+      <li><a href="/user/3/edit">edit user 3</a></li>
+      <li><a href="/foo/5/bar">foo 5 bar</a></li>
+      <li><a href="/foo/bar/foo/bar">long route example</a></li>
+      <li><a href="/contact-form">contact form</a></li>
+      <li><a href="/get-post-sample">get+post example</a></li>
+      <li><a href="/test.html">test.html</a></li>
+      <li><a href="/aTrailingSlashDoesNotMatter">aTrailingSlashDoesNotMatter</a></li>
+      <li><a href="/aTrailingSlashDoesNotMatter/">aTrailingSlashDoesNotMatter/</a></li>
+      <li><a href="/theCaseDoesNotMatter">theCaseDoesNotMatter</a></li>
+      <li><a href="/thecasedoesnotmatter">thecasedoesnotmatter</a></li>
+      <li><a href="/this-route-is-not-defined">404 Test</a></li>
+      <li><a href="/this-route-is-defined">405 Test</a></li>
+  </ul>
 EOD;
+}
 
 // Include router class
 include 'Route.php';
 
 // Add base route (startpage)
 Route::add('/', function() {
+  navi();
   echo 'Welcome :-)';
 });
 
 // Another base route example
 Route::add('/index.php', function() {
+  navi();
   echo 'You are not really on index.php ;-)';
 });
 
 // Simple test route that simulates static html file
 // TODO: Fix this for some web servers
 Route::add('/test.html', function() {
+  navi();
   echo 'Hello from test.html';
 });
 
 // Post route example
 Route::add('/contact-form', function() {
+  navi();
   echo '<form method="post"><input type="text" name="test"><input type="submit" value="send"></form>';
 }, 'get');
 
 // Post route example
 Route::add('/contact-form', function() {
+  navi();
   echo 'Hey! The form has been sent:<br>';
   print_r($_POST);
 }, 'post');
 
 // Get and Post route example
 Route::add('/get-post-sample', function() {
+  navi();
 	echo 'You can GET this page and also POST this form back to it';
 	echo '<form method="post"><input type="text" name="input"><input type="submit" value="send"></form>';
 	if (isset($_POST['input'])) {
@@ -64,16 +72,19 @@ Route::add('/get-post-sample', function() {
 // Also users could inject SQL statements or other untrusted data if you use (.*)
 // You should better use a saver expression like /user/([0-9]*)/edit or /user/([A-Za-z]*)/edit
 Route::add('/user/(.*)/edit', function($id) {
+  navi();
   echo 'Edit user with id '.$id.'<br>';
 });
 
 // Accept only numbers as parameter. Other characters will result in a 404 error
 Route::add('/foo/([0-9]*)/bar', function($var1) {
+  navi();
   echo $var1.' is a great number!';
 });
 
 // Crazy route with parameters
 Route::add('/(.*)/(.*)/(.*)/(.*)', function($var1,$var2,$var3,$var4) {
+  navi();
   echo 'This is the first match: '.$var1.' / '.$var2.' / '.$var3.' / '.$var4.'<br>';
 });
 
@@ -86,27 +97,32 @@ Route::add('/foo/bar/foo/bar', function() {
 
 // Trailing slash example
 Route::add('/aTrailingSlashDoesNotMatter', function() {
+  navi();
   echo 'a trailing slash does not matter<br>';
 });
 
 // Case example
-Route::add('/theCaseDoesNotMatter',function(){
+Route::add('/theCaseDoesNotMatter',function() {
+  navi();
   echo 'the case does not matter<br>';
 });
 
 // 405 test
 Route::add('/this-route-is-defined', function() {
+  navi();
   echo 'You need to patch this route to see this content';
 }, 'patch');
 
 // Add a 404 not found route
 Route::pathNotFound(function($path) {
+  navi();
   echo 'Error 404 :-(<br>';
   echo 'The requested path "'.$path.'" was not found!';
 });
 
 // Add a 405 method not allowed route
 Route::methodNotAllowed(function($path, $method) {
+  navi();
   echo 'Error 405 :-(<br>';
   echo 'The requested path "'.$path.'" exists. But the request method "'.$method.'" is not allowed on this path!';
 });
